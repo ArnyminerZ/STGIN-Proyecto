ca3b73b94abc5bfc10807a2b43ba296e08a9ac70
Arnau Mora Gras <arnyminerz@proton.me>
2024-12-14 21:16:28 +0100

    HTML root

--- src/main/kotlin/com/arnyminerz/upv/Routing.kt
+++ src/main/kotlin/com/arnyminerz/upv/Routing.kt
@@ -1,6 +1,11 @@
 package com.arnyminerz.upv
 
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
+    routing {
+        staticResources("/", "web")
+    }
 }
