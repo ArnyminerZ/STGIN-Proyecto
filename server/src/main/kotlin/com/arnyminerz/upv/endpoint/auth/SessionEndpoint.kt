329c14e26667e4ba52b41c76f60b2a89225e3367
Arnau Mora <arnyminerz@proton.me>
2025-02-18 17:42:46 +0100

    Fixed endpoint path

--- server/src/main/kotlin/com/arnyminerz/upv/endpoint/auth/SessionEndpoint.kt
+++ server/src/main/kotlin/com/arnyminerz/upv/endpoint/auth/SessionEndpoint.kt
@@ -1,5 +1,6 @@
 package com.arnyminerz.upv.endpoint.auth
 
+import Endpoints
 import com.arnyminerz.upv.endpoint.type.EndpointContext
 import com.arnyminerz.upv.endpoint.type.SecureEndpoint
 import io.ktor.http.HttpMethod
@@ -8,7 +9,7 @@ import io.ktor.http.HttpStatusCode
 /**
  * Checks whether the user is logged in or not.
  */
-object SessionEndpoint : SecureEndpoint("/api/auth/session", HttpMethod.Get) {
+object SessionEndpoint : SecureEndpoint(Endpoints.Auth.SESSION, HttpMethod.Get) {
     override suspend fun EndpointContext.secureBody(userId: String) {
         respondSuccess(userId, HttpStatusCode.OK)
     }
--- shared/src/commonMain/kotlin/Endpoints.kt
+++ shared/src/commonMain/kotlin/Endpoints.kt
@@ -5,6 +5,7 @@ object Endpoints {
         const val LOGIN = "/auth/login"
         const val REGISTER = "/auth/register"
         const val LOGOUT = "/auth/logout"
+        const val SESSION = "/auth/session"
     }
 
     object Game {
